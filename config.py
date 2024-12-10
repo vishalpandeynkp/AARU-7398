9f6a45f85862a070dcac02bd58fa920316a89105
SukoonOfficial <linkkiduniyaofficial@gmail.com>
2024-12-10 15:16:59 +0530

    config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 18000))
 
 # Chat id of a group for logging bot's activities
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
@@ -32,21 +32,21 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
+    "https://github.com/SukoonOfficial/SukoonMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexGenBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NexGenSpam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SukoonUpdates")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SukoonChats")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 # make your bots privacy from telegra.ph and put your url here 
-PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
+PRIVACY_LINK = getenv("PRIVACY_LINK", "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg")
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
@@ -55,7 +55,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 20000))
 
 
 # Telegram audio and video file size limit (in bytes)
@@ -81,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
+    "START_IMG_URL", "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
+    "PING_IMG_URL", "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
-STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+STATS_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+TELEGRAM_AUDIO_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+TELEGRAM_VIDEO_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+STREAM_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+SOUNCLOUD_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+YOUTUBE_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/ydLZ5RN/IMG-20240316-031033-252.jpg"
 
 
 def time_to_seconds(time):
